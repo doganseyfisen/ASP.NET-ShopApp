aa528b0bbfb52b4151fb1ddcb91c04a862cc5344
Dogan Seyfi Sen <doganseyfisen@gmail.com>
2024-07-22 23:46:31 +0300

    created entities and repositories project

---
+++ Repositories/RepositoryContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories;
+public class RepositoryContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Product>().HasData(
+            new Product() { ProductId = 1, ProductName = "GTA V", ProductPrice = 14.98m },
+            new Product() { ProductId = 2, ProductName = "The Witcher 3", ProductPrice = 29.99m },
+            new Product() { ProductId = 3, ProductName = "Celeste", ProductPrice = 10.49m },
+            new Product() { ProductId = 4, ProductName = "Paper, Please", ProductPrice = 9.99m },
+            new Product() { ProductId = 5, ProductName = "Don't Starve", ProductPrice = 3.29m }
+        );
+    }
+}
