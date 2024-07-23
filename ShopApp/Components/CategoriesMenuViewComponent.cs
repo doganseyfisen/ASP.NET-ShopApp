c3c0b22d59c1908e51518d4d48618b0bbe92a954
Dogan Seyfi Sen <doganseyfisen@gmail.com>
2024-07-23 23:10:16 +0300

    created ViewComponent

---
+++ ShopApp/Components/CategoriesMenuViewComponent.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace ShopApp.Components
+{
+    public class CategoriesMenuViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _manager;
+
+        public CategoriesMenuViewComponent(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _manager.CategoryService.GetAllCategories(false);
+
+            return View(categories);
+        }
+    }
+}
