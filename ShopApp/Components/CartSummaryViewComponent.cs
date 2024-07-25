83bb0bd34102a06292efffe438a8ad7e023226ed
Dogan Seyfi Sen <doganseyfisen@gmail.com>
2024-07-25 15:36:51 +0300

    added cart summary view component and some icons for navbar

---
+++ ShopApp/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly Cart _cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            _cart = cartService;
+        }
+
+        public string Invoke()
+        {
+            return _cart.Lines.Count().ToString();
+        }
+    }
+}
